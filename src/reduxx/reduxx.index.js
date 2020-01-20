f33ef4140fdc76ab144fd216e45e2e6b36b6911e
Michal Oboril <Michal.Oboril@dixonscarphone.com>
2020-01-20 14:44:40 +0100

    Setup

--- src/reduxx/reduxx.index.js
+++ src/reduxx/reduxx.index.js
@@ -1,9 +1,39 @@
+import { getRandomString, getRandInt } from '../utils/getRandomString';
+
+const smallChild = (name) => {
+  console.log(name);
+  const root = document.createElement('div');
+  const nameContainer = document.createElement('span');
+  nameContainer.textContent = name;
+  const nameContainerAgain = document.createElement('span');
+  nameContainerAgain.textContent = name;
+  root.appendChild(nameContainer);
+  root.appendChild(nameContainerAgain);
+  return root;
+};
 
 
+const mother = (nChilds) => {
+  const root = document.createElement('div');
+  root.setAttribute('class', 'motherFucker');
+  console
+  new Array(nChilds).map((x) => {
+    console.log('pice!');
+    const child = smallChild(getRandomString());
+    root.appendChild(child);
+  });
+  return root;
+};
 
 const appRoot = document.createElement('div');
-appRoot.setAttribute('id','reduxxApp');
+appRoot.setAttribute('id', 'reduxxApp');
 appRoot.textContent = 'Hovno!!!!';
 document.body.appendChild(appRoot);
 
+const rerender = () => {
+  const nChilds = getRandInt(1, 8);
+  appRoot.innerHTML='';
+  appRoot.appendChild(mother(nChilds));
+};
 
+setInterval(rerender, 5000);
